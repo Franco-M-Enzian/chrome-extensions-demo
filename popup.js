3b6473ce1dc7aedb68ab13eb702789c3fc12bb0b
Franco-M-Enzian <emf0000b@gmail.com>
2024-11-27 00:21:53 +0900

    Changed Action - Add Summer

--- popup.js
+++ popup.js
@@ -1,20 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 冬をクリック時の処理
-  document.getElementById("winter").addEventListener("click", () => {
-    console.log("onclick: 冬");
-    sendMessageToContentScript("maple");
-  });
-
   // 春をクリック時の処理
   document.getElementById("spring").addEventListener("click", () => {
     console.log("onclick: 春");
     sendMessageToContentScript("spring");
   });
 
+  // 冬をクリック時の処理
+  document.getElementById("summer").addEventListener("click", () => {
+    console.log("onclick: 夏");
+    sendMessageToContentScript("summer");
+  });
+
   // 秋をクリック時の処理
   document.getElementById("autumn").addEventListener("click", () => {
     console.log("onclick: 秋");
     sendMessageToContentScript("autumn");
+  });
+
+  // 冬をクリック時の処理
+  document.getElementById("winter").addEventListener("click", () => {
+    console.log("onclick: 冬");
+    sendMessageToContentScript("winter");
   });
 
   function sendMessageToContentScript(theme) {
